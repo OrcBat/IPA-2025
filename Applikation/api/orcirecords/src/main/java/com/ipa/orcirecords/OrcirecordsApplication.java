a32c336156f884b7385e309a1703f40750fe2db2
OrcBat <89592618+OrcBat@users.noreply.github.com>
2025-02-24 21:12:45 +0100

    initial project setup and documentation

---
+++ Applikation/api/orcirecords/src/main/java/com/ipa/orcirecords/OrcirecordsApplication.java
@@ -0,0 +1,13 @@
+package com.ipa.orcirecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcirecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcirecordsApplication.class, args);
+	}
+
+}
---
+++ Applikation/api/orcirecords/src/test/java/com/ipa/orcirecords/OrcirecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipa.orcirecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcirecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
